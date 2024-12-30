d10881e89fdfa86f35aa04320bb353f4e1eccc85
manasdesai <desaimanas808@gmail.com>
2024-12-30 10:38:31 +0530

    Added the Barista Robot project

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <stdexcept>
+
+
+namespace driving{
+    class identifier{
+
+       
+        public:
+        identifier(ushort day, ushort month, ushort year) : day_{day}, month_{month}, year_{year} {
+            day_=day;
+
+        }
+
+        private:
+            ushort day_;
+            ushort month_;
+            ushort year_;
+        protected:
+
+        void university::Date::print_date() const{
+            std::cout << "Hello World" <<;
+        }
+        ushort& get_day() const {return day_;}
+        ushort get_day() const {
+            return day_;
+        }
+
+        void set_day(ushort day){day_=day;}
+    };
+}
+
+void university::Date::print_date const(){
+
+}
+
+int main(){
+
+    try{
+        int result = divide(numerator, denominator);
+
+    } catch( const std::invalid_argument& e)
+    driving::identifier id;
+    auto u = std::make_unique<university::Date>(); //make a unique pointer
+    auto u_date = std::make_unique<driving::identifier>();
+    std::cout << u_date->get_day() << '\n';
+    auto u_date = std::make_unique<driving::identifier>();
+    std::cout << u_date->get_day() << '\n';
+
+}
